a72ad575404135a6d2267f71862adba02db9e91a
whenes <whenes.oliveira@gmail.com>
2022-07-16 15:03:22 -0300

    Aplicado teste com Exception

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -7,14 +7,19 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BonusServiceTest {
     @Test
     void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Funcionario 1", LocalDate.now(), new BigDecimal("25000")));
-        assertEquals(BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP), bonus);
+//        assertThrows(IllegalArgumentException.class, () -> service.calcularBonus(new Funcionario("Funcionario 1", LocalDate.now(), new BigDecimal("25000"))));
+        try {
+            service.calcularBonus(new Funcionario("Funcionario 1", LocalDate.now(), new BigDecimal("25000")));
+            fail("Não deu a exception!");
+        } catch (Exception e) {
+            assertEquals("Funcionário com salário maior que 10000 reais não pode receber bônus!", e.getMessage());
+        }
     }
 
     @Test
